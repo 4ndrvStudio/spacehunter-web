112a1a213dc7bd2ee43a50e068e29ff6677af13d
4ndrvStudio <4ndrv.Studio@gmail.com>
2024-02-29 15:32:13 +0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -10,6 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// const options = {
+//   key: fs.readFileSync('server.key'),
+//   cert: fs.readFileSync('server.cert')
+// };
+
+// const server = https.createServer(options, app);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on Port:${PORT}`);
+  console.log(`Server is running on port:${PORT}`);
 });
